277829ab43acfcd762c3d17f093103e401379d92
Alec Robinson <s4203312@glos.ac.uk>
2023-01-20 12:40:29 +0000

    l

---
+++ Assets/Week 1 - Maze/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	//Variables for the instantiation of the maze
+	public Maze mazePrefab;
+	private Maze mazeInstance;
+
+
+	private void Start()
+	{
+		BeginGame();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			RestartGame();
+		}
+	}
+
+	
+	//Functions for creating and clearing the maze
+	private void BeginGame()
+	{
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+	}
+
+	private void RestartGame()
+	{
+		Destroy(mazeInstance.gameObject);
+		BeginGame();
+	}
+}
