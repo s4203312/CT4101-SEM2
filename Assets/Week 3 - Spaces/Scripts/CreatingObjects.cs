112f6a96f307d56f5355b893132d92b7dad23dae
Alec Robinson <s4203312@glos.ac.uk>
2023-02-17 12:25:38 +0000

    Week5 work

--- Assets/Week 3 - Spaces/Scripts/CreatingObjects.cs
+++ Assets/Week 3 - Spaces/Scripts/CreatingObjects.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 public class CreatingObjects : MonoBehaviour
 {
     [SerializeField] public GameObject star;
-    [SerializeField] public GameObject planet;
+    //[SerializeField] public GameObject planet;
+    public GameObject currentStar;
+    //public GameObject currentPlanet;
 
-    //[SerializeField] 
+    [SerializeField] public GameObject[] array;
+    [SerializeField] public string[] starNames;
+
+
 
     void Start()
     {
-        for (var i = 0; i < 10; i++)
-        {
-            
-
-            var positionStar = new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f));
-            GameObject currentStar = Instantiate(star, positionStar, Quaternion.identity);
-
-            //List < GameObject >> solarSystems = new List<GameObject>();
-            //solarSystems.Add(currentStar);
-
-            for (var j = 0; j < 3; j++)
-            {
-                var positionPlanet = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f));
-                GameObject currentPlanet = Instantiate(planet, positionPlanet + currentStar.transform.position, Quaternion.identity, currentStar.transform);
-                //solarSystems.Add(currentPlanet);
-            }
-
-            //List<List<GameObject>> galaxy = new List<List<GameObject>>(solarSystem[i]);
-        }
-        //foreach (GameObject solarsystem in solarSystems) {
-        //    Debug.Log(solarsystem.name);
-        //}
+        Creation();
+        //Orbiting();
     }
 
-    private void Update() {
+    private void Update() 
+    {
         
     }
 
-    //private void Orbiting() {
-    //    foreach (GameObject solarsystem in solarSystems) {
-    //        Debug.Log(solarsystem.name);
+    private void Creation()
+    {
+        array = new GameObject[10];
+
+        for (var i = 0; i < 10; i++)
+        {
+            var positionStar = new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f));
+            array[i] = Instantiate(star, positionStar, Quaternion.identity);
+            array[i].transform.tag = "Star";
+            array[i].transform.name = starNames[Random.Range(0, starNames.Length)] + " " + i;
+
+
+            //for (var j = 0; j < 3; j++)
+            //{
+            //    var positionPlanet = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f));
+            //    currentPlanet = Instantiate(planet, positionPlanet + currentStar.transform.position, Quaternion.identity, currentStar.transform);
+
+            //    array[i, j] = currentPlanet;
+            //}
+        }
+    }
+
+    //private void Orbiting() 
+    //{
+    //    foreach(var item in array)
+    //    {
+    //        Debug.Log(item.ToString());
     //    }
     //}
 }
