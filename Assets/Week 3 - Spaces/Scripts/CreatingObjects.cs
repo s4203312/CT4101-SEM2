d7c7381b535edc3bb38112657b2173df41f8ec2f
s4203312 <s4203312@glos.ac.uk>
2023-02-19 18:47:48 +0000

    Orbiting

--- Assets/Week 3 - Spaces/Scripts/CreatingObjects.cs
+++ Assets/Week 3 - Spaces/Scripts/CreatingObjects.cs
@@ -1,57 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class CreatingObjects : MonoBehaviour
 {
+    //Stars
     [SerializeField] public GameObject star;
-    //[SerializeField] public GameObject planet;
-    public GameObject currentStar;
-    //public GameObject currentPlanet;
-
-    [SerializeField] public GameObject[] array;
+    [SerializeField] public GameObject[] starArray;
     [SerializeField] public string[] starNames;
 
+    //Planets
+    [SerializeField] public GameObject planet;
+    [SerializeField] public GameObject[] planetArray;
+    [SerializeField] public string[] planetNames;
+    private int planetCounter = 0;
 
+    //Orbiting
+    [SerializeField] public float orbitSpeed;
+    private GameObject planetSetting;
+    Vector3[] rotations = new Vector3[] { Vector3.up, Vector3.left, Vector3.right, Vector3.down };
+    [SerializeField] public List<Vector3> rotationDirection;
+    private int rotationCounter = 0;
+
+    //Rendering
+    [SerializeField] public Material[] mat;
 
     void Start()
     {
         Creation();
-        //Orbiting();
+        Rendering();
     }
 
     private void Update() 
     {
-        
+        Orbiting();
     }
 
     private void Creation()
     {
-        array = new GameObject[10];
+        starArray = new GameObject[10];
+        planetArray = new GameObject[30];
+        rotationDirection = new List<Vector3>();
 
         for (var i = 0; i < 10; i++)
         {
-            var positionStar = new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f), Random.Range(-200.0f, 200.0f));
-            array[i] = Instantiate(star, positionStar, Quaternion.identity);
-            array[i].transform.tag = "Star";
-            array[i].transform.name = starNames[Random.Range(0, starNames.Length)] + " " + i;
+            var positionStar = new Vector3(Random.Range(-1000.0f, 1000.0f), Random.Range(-1000.0f, 1000.0f), Random.Range(-1000.0f, 1000.0f));
+            starArray[i] = Instantiate(star, positionStar, Quaternion.identity);
+            starArray[i].transform.tag = "Star";
+            starArray[i].transform.name = starNames[Random.Range(0, starNames.Length)] + " " + i;
 
-
-            //for (var j = 0; j < 3; j++)
-            //{
-            //    var positionPlanet = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f));
-            //    currentPlanet = Instantiate(planet, positionPlanet + currentStar.transform.position, Quaternion.identity, currentStar.transform);
-
-            //    array[i, j] = currentPlanet;
-            //}
+            for (var j = 0; j < 3; j++)
+            {
+                var positionPlanet = new Vector3(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
+                planetArray[planetCounter] = Instantiate(planet, positionPlanet + positionStar, Quaternion.identity, starArray[i].transform);
+                planetArray[planetCounter].transform.tag = "Planet";
+                planetArray[planetCounter].transform.name = planetNames[Random.Range(0, planetNames.Length)] + " " + planetCounter;
+                planetCounter += 1;
+                rotationDirection.Add(rotations[Random.Range(0, rotations.Length)]);
+            }
         }
     }
 
-    //private void Orbiting() 
-    //{
-    //    foreach(var item in array)
-    //    {
-    //        Debug.Log(item.ToString());
-    //    }
-    //}
+    private void Rendering() {
+        foreach (var star in starArray) {
+            star.GetComponent<Renderer>().material = mat[Random.Range(0, mat.Length)];
+        }
+        foreach (var planet in planetArray) {
+            planet.GetComponent<Renderer>().material = mat[Random.Range(0, mat.Length)];
+        }
+    }
+
+    private void Orbiting() 
+    {
+        foreach (var star in starArray) {
+            for(var i = 0; i < 3; i++) {
+                planetSetting = star.gameObject.transform.GetChild(i).gameObject;
+                Vector3 pos = star.transform.localPosition;
+                planetSetting.transform.RotateAround(pos, rotationDirection[rotationCounter], Random.Range(5.0f,30.0f) * Time.deltaTime);
+                rotationCounter += 1;
+            }
+        }
+        rotationCounter = 0;
+    }
 }
